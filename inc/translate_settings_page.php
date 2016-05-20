4d25db6a7aad80ca232bc4dd3e9ae4266e718960
markphp <vinexx1@mail.ru>
2016-05-21 01:00:41 +0300

    add same stile

--- inc/translate_settings_page.php
+++ inc/translate_settings_page.php
@@ -14,58 +14,41 @@ function translate_settings_page(){
 		<a href="" class="add_language">Add New language</a>
 		<a href="" class="add_language">Add New translate</a>
 	</div>
-	<div class="translate_table">
+	<div class="translate_table"></div>
+	<div class="forms_layout">
+		<form id="form" method="post" action="javascript:void(0);" onsubmit="get_all()">
+			<p>Name:</p>
+			<p><input type="text" name="name" class="name" value="static_translate" required/></p>
 
-		<table width="100%">
-			<thead class="thead">
-			<tr>
+			<p><input type="submit" name="send" class="send" value="senden"></p>
+		</form>
 
-				<th></th>
+		<form id="update_form" method="post" action="javascript:void(0);" onsubmit="update()">
+			<p>ID:</p><input name="row_id" id="row_id">
+			<p>en_US:</p>
+			<p><input type="text" name="en_US" id="en_US" value="" required/></p>
+			<p>zh_HK:</p>
+			<p><input type="text" name="ru_RU" id="ru_RU" value="" required/></p>
 
-			</tr>
-			</thead>
+			<p><input type="submit" name="send" class="send" value="update"></p>
+		</form>
 
-			<tr class="data_row">
-				<td></td>
+		<form id="insert_form" method="post" action="javascript:void(0);" onsubmit="set_row()">
+			<p>en_US:</p>
+			<p><input type="text" name="en_US" id="en_US" value="" required/></p>
+			<p>zh_HK:</p>
+			<p><input type="text" name="zh_HK" id="zh_HK" value="" required/></p>
 
-			</tr>
+			<p><input type="submit" name="send" class="send" value="insert"></p>
+		</form>
 
+		<form id="add_coll" method="post" action="javascript:void(0);" onsubmit="set_col()">
+			<p>Name:</p>
+			<p><input type="text" name="name" class="name" value="" required/></p>
 
-		</table>
+			<p><input type="submit" name="send" class="send" value="senden"></p>
+		</form>
 
 	</div>
-
-	<form id="form" method="post" action="javascript:void(0);" onsubmit="get_all()">
-		<p>Name:</p>
-		<p><input type="text" name="name" class="name" value="static_translate" required/></p>
-
-		<p><input type="submit" name="send" class="send" value="senden"></p>
-	</form>
-
-	<form id="update_form" method="post" action="javascript:void(0);" onsubmit="update()">
-		<p>ID:</p><input name="row_id" id="row_id">
-		<p>en_US:</p>
-		<p><input type="text" name="en_US" id="en_US" value="" required/></p>
-		<p>zh_HK:</p>
-		<p><input type="text" name="ru_RU" id="ru_RU" value="" required/></p>
-
-		<p><input type="submit" name="send" class="send" value="update"></p>
-	</form>
-
-	<form id="insert_form" method="post" action="javascript:void(0);" onsubmit="set_row()">
-		<p>en_US:</p>
-		<p><input type="text" name="en_US" id="en_US" value="" required/></p>
-		<p>zh_HK:</p>
-		<p><input type="text" name="zh_HK" id="zh_HK" value="" required/></p>
-
-		<p><input type="submit" name="send" class="send" value="insert"></p>
-	</form>
-
-	<form id="add_coll" method="post" action="javascript:void(0);" onsubmit="set_col()">
-		<p>Name:</p>
-		<p><input type="text" name="name" class="name" value="" required/></p>
-
-		<p><input type="submit" name="send" class="send" value="senden"></p>
-	</form>
 <?php
 }
